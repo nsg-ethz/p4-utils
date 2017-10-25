71044c03a62e56c6bd3a404ef2ae194d6666ebc8
edgarcosta92 <cedgar@student.ethz.ch>
2017-10-25 18:01:00 +0200

    implementing custom cli for p4 switches in mininet

--- mininet/cli.py
+++ mininet/cli.py
@@ -8,89 +8,52 @@ class P4CLI(CLI):
         CLI.__init__(self,*args,**kwargs)
 
 
-    def do_p4(self,line=""):
-        """Start/stop/restart the mininet generator
-           Usage: mininet start/stop/restart node
-        """
-        args = line.strip().split()
-        if len(args) == 2:
-            cmd = args[0]
-            host = args[1]
+    def do_p4switch_stop(self,line=""):
 
-            if cmd == "start":
-                if host == "all":
-                    self.minigenerator.start()
-                elif host in self.minigenerator.mininet:
-                    self.minigenerator.start_node(host)
-                else:
-                    error('invalid host')
-            elif cmd == "stop":
-                if host == "all":
-                    self.minigenerator.stop()
-                elif host in self.minigenerator.mininet:
-                    self.minigenerator.stop_node(host)
-                else:
-                    error('invalid host')
-            elif cmd == "restart":
-                if host == "all":
-                    self.minigenerator.restart()
-                elif host in self.minigenerator.mininet:
-                    self.minigenerator.restart_node(host)
-                else:
-                    error('invalid host')
-            else:
-                error('invalid command: start stop restart')
-
+        """stop simple switch from switch namespace"""
+        switch_name = line.split()
+        if not switch_name or len(switch_name) > 1:
+            error('usage: p4switch_stop <p4switch name>\n')
         else:
-            error('invalid number of args: minigenerator cmd node')
+            switch_name = switch_name[0]
+            if switch_name not in self.mn:
+                error("p4switch %s not in the network\n" % switch_name)
+            else:
+                p4switch = self.mn[switch_name]
+                p4switch.p4switch.delete()
 
+    def do_p4switch_start(self,line=""):
 
-    # def do_stoprouter(self,line=""):
-    #
-    #     """stop zebra and ospf from a router"""
-    #     router_name = line.split()
-    #     if not router_name or len(router_name) > 1:
-    #         error('usage: stoprouter router\n')
-    #     else:
-    #         router_name = router_name[0]
-    #         if router_name not in self.mn:
-    #             error("router %s not in the network\n" % router_name)
-    #         else:
-    #             router = self.mn[router_name]
-    #             router.router.delete()
-    #
-    # def do_startrouter(self,line=""):
-    #
-    #     """start zebra and ospf from a router"""
-    #     router_name = line.split()
-    #     if not router_name or len(router_name) > 1:
-    #         error('usage: startrouter router\n')
-    #     else:
-    #         router_name = router_name[0]
-    #         if router_name not in self.mn:
-    #             error("router %s not in the network\n" % router_name)
-    #         else:
-    #             router = self.mn[router_name]
-    #             router.router.start()
-    #
-    # def do_rebootquaggas(self, line=""):
-    #     """restarts zebra and ospfd from all routers"""
-    #
-    #     for r in self.mn.routers:
-    #         r.router.delete()
-    #         r.router.start()
-    #
-    # def do_rebootquagga(self,line=""):
-    #
-    #     """restarts zebra and ospf from a router"""
-    #     router_name = line.split()
-    #     if not router_name or len(router_name) > 1:
-    #         error('usage: rebootquagga router\n')
-    #     else:
-    #         router_name = router_name[0]
-    #         if router_name not in self.mn:
-    #             error("router %s not in the network\n" % router_name)
-    #         else:
-    #             router = self.mn[router_name]
-    #             router.router.delete()
-    #             router.router.start()
+        """start again simple switch from namespace"""
+        switch_name = line.split()
+        if not switch_name or len(switch_name) > 1:
+            error('usage: p4switch_start <p4switch name>\n')
+        else:
+            switch_name = switch_name[0]
+            if switch_name not in self.mn:
+                error("p4switch %s not in the network\n" % switch_name)
+            else:
+                p4switch = self.mn[switch_name]
+                p4switch.p4switch.start()
+
+    def do_p4switches_reboot(self, line=""):
+        """reboot all p4 switches with new program """
+
+        for r in self.mn.p4switchs:
+            r.p4switch.delete()
+            r.p4switch.start()
+
+    def do_p4switch_reboot(self,line=""):
+
+        """reboot a p4 switch with new program"""
+        switch_name = line.split()
+        if not switch_name or len(switch_name) > 1:
+            error('usage: p4switch_reboot <p4switch name>\n')
+        else:
+            switch_name = switch_name[0]
+            if switch_name not in self.mn:
+                error("p4switch %s not in the network\n" % switch_name)
+            else:
+                p4switch = self.mn[switch_name]
+                p4switch.p4switch.delete()
+                p4switch.p4switch.start()
