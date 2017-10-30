2ed6679003bcf6328bad2ea1f447baa8ef2533a2
edgarcosta92 <cedgar@student.ethz.ch>
2017-10-30 16:34:43 +0100

    forgot to add p4net code

---
+++ mininet/p4net.py
@@ -0,0 +1,25 @@
+from mininet.net import Mininet
+
+class P4Mininet(Mininet):
+
+    def __init__(self, *args, **kwargs):
+        """
+        Adds p4switches
+        :param args:
+        :param kwargs:
+        """
+        self.p4switches = []
+        super(P4Mininet, self).__init__(*args, **kwargs)
+
+    def build( self ):
+        """
+        Build mininet
+        :return:
+        """
+
+        super(P4Mininet, self).build()
+
+        for switch in self.switches:
+            name = switch.name
+            if self.topo.isP4Switch(name):
+                self.p4switches.append( switch )
