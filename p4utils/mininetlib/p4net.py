021b94240fff8746cbf29236f684a53cd4e6b156
edgar-costa <cedgar@ethz.ch>
2019-10-24 03:00:04 +0200

    switch to switch interfaces have 20 bytes more. This is needed when a host sends a packet as big as the MTU and switches need to add new headers, otherwise packets would get dropped. Keep in mind that this only works if the switch removes the extra header before reaching a host, if the header has to be kept... then something smarter has to be done?..

--- p4utils/mininetlib/p4net.py
+++ p4utils/mininetlib/p4net.py
@@ -21,12 +21,16 @@ class P4Mininet(Mininet):
     def start(self):
         super(P4Mininet, self).start()
 
+        hosts_mtu = 9500
+        # Trick to allow switches to add headers
+        # when packets have the max MTU
+        switches_mtu = 9520
+
         #remove Ipv6 for all the interfaces
         for link in self.links:
-
             cmd1 = "/sbin/ethtool --offload {0} rx off tx off sg off"
             cmd2 = "sysctl net.ipv6.conf.{0}.disable_ipv6=1"
-            cmd3 = "ip link set {0} mtu 9500"
+            cmd3 = "ip link set {} mtu {}"
 
             #execute the ethtool command to remove some offloads
             link.intf1.cmd(cmd1.format(link.intf1.name))
@@ -36,6 +40,14 @@ class P4Mininet(Mininet):
             link.intf1.cmd(cmd2.format(link.intf1.name))
             link.intf2.cmd(cmd2.format(link.intf2.name))
 
-            #increase mtu to 9500 (jumbo frames)
-            link.intf1.cmd(cmd3.format(link.intf1.name))
-            link.intf2.cmd(cmd3.format(link.intf2.name))
+            #increase mtu to 9500 (jumbo frames) for switches we do it special
+            node1_is_host = link.intf1.node in self.hosts
+            node2_is_host = link.intf2.node in self.hosts
+
+            if node1_is_host or node2_is_host:
+                mtu = hosts_mtu
+            else:
+                mtu = switches_mtu
+
+            link.intf1.cmd(cmd3.format(link.intf1.name, mtu))
+            link.intf2.cmd(cmd3.format(link.intf2.name, mtu))
