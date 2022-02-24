904f2f5b1eee68e33cfc36e3e67aec20e4ec5967
edgar-costa <cedgar@ethz.ch>
2022-02-24 10:53:51 +0000

    Added an example of how to use p4-utils with tofino model switches

--- examples/tofino/build/CMakeFiles/heavy_hitter-tofino.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/heavy_hitter-tofino"
-  "heavy_hitter/tofino/bf-rt.json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/heavy_hitter-tofino.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
